464ba931d48f8b12e888b5962a8d90eef8121194
Tinoxxxmit3x <widmert@bzz.ch>
2024-08-26 15:48:45 +0200

    -

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-# Dein Code kommt hier hin
+"""
+This function adjusts the ingredients of a recipe based on the number of servings.
+"""
+import json
+
+def adjust_recipe(recipe, servings):
+    for ingredient in recipe['ingredients']:
+        recipe['ingredients'][ingredient] = recipe['ingredients'][ingredient] / recipe['servings'] * servings
+    recipe['servings'] = servings
+    return recipe
+
+
+def load_recipe(my_json):
+    return json.loads(my_json)
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    recipe_json = ('{"title": "Spaghetti Bolognese", "ingredients": '
+                   '{"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}')
+    print(adjust_recipe(load_recipe(recipe_json), 2))
